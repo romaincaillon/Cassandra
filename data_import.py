be988ad4fd4d9a9fb101d13591588fcbc233e9aa
Romain Caillon <romain.caillon@devinci.fr>
2019-01-24 01:40:08 +0100

    add bad arguments exceptions

--- data_import.py
+++ data_import.py
@@ -6,13 +6,19 @@ from cassandra.cluster import Cluster
 cluster = Cluster()
 session = cluster.connect('dblp')
 
-with open(sys.argv[1]) as f:
-    count = 0
-    for line in f:
-        line = line.replace('"_id"', '"id"').replace("'", "''")
-        query = "INSERT INTO bibliography JSON '{}'".format(line)
-        session.execute(query)
-        count += 1
-        if (count % 1000) == 0:
-            print(count)
+try:
+    with open(sys.argv[1]) as f:
+        count = 0
+        for line in f:
+            line = line.replace('"_id"', '"id"').replace("'", "''")
+            query = "INSERT INTO bibliography JSON '{}'".format(line)
+            session.execute(query)
+            count += 1
+            if (count % 1000) == 0:
+                print(count)
 
+except FileNotFoundError:
+    print("Invalid file")
+
+except IndexError:
+    print("Usage: " + sys.argv[0] + " 'file'")
