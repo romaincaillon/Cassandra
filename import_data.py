567c16c7700818bbf2d520423efda1b0e19fda30
Romain Caillon <romain.caillon@devinci.fr>
2019-01-24 18:05:27 +0100

    change type from double to int in database and add corresponding modifications in python script

--- import_data.py
+++ import_data.py
@@ -2,6 +2,7 @@
 
 import sys
 from cassandra.cluster import Cluster
+import json
 
 cluster = Cluster()
 session = cluster.connect('dblp')
@@ -10,6 +11,20 @@ try:
     with open(sys.argv[1]) as f:
         count = 0
         for line in f:
+            dictio = json.loads(line)
+            try:
+                dictio["year"] = int(dictio["year"])
+            except TypeError:
+                pass
+            try:
+                dictio["pages"]["start"] = int(dictio["pages"]["start"])
+            except TypeError:
+                pass
+            try:
+                dictio["pages"]["end"] = int(dictio["pages"]["end"])
+            except TypeError:
+                pass
+            line = json.dumps(dictio)
             line = line.replace('"_id"', '"id"').replace("'", "''")
             query = "INSERT INTO bibliography JSON '{}'".format(line)
             session.execute(query)
